65c98e8b4b9034b6b0d08a9ecd960930d60cc7c9
Nikhil124588 <61134484+Nikhil124588@users.noreply.github.com>
2020-02-28 03:08:07 +0530

    Update Student.java

--- BufRead/src/com/cjc/bufferedwriter/Student.java
+++ BufRead/src/com/cjc/bufferedwriter/Student.java
@@ -2,7 +2,7 @@ package com.cjc.bufferedwriter;
 
 public class Student {
 private int id;
-private String name;
+private String name1;
 private String addr;
 public int getId() {
 	return id;
@@ -13,8 +13,8 @@ public void setId(int id) {
 public String getName() {
 	return name;
 }
-public void setName(String name) {
-	this.name = name;
+public void setName(String name1) {
+	this.name = name1;
 }
 public String getAddr() {
 	return addr;
